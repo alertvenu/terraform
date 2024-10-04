483d2b7b245e0dd0476d74467be2e30e948487e6
alertvenu <alertvenu@gmail.com>
2024-10-04 12:36:16 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access
